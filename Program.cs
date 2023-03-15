ab2da027888df45fd2466774d2251eaf83aef011
Igor Bodalov <igorbodalov@gmail.com>
2023-03-15 15:49:20 +0300

    HelloConsoleWithVariants

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
-﻿int numberA = new Random().Next(1, 10);
-Console.WriteLine(numberA);
-int numberB = new Random().Next(1, 10);
-Console.WriteLine(numberB);
-int result = numberA + numberB;
-Console.WriteLine(result);
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
